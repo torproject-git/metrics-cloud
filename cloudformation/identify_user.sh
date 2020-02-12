e9fbd32bd802e9f5411fa8085b35c82f482ef48b
Ana Custura <ana@netstat.org.uk>
2020-02-12 23:12:50 +0000

    Change script to zsh

--- cloudformation/identify_user.sh
+++ cloudformation/identify_user.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/bin/zsh
 #aws ec2 describe-key-pairs | jq -r '.KeyPairs[].KeyName' | grep `aws iam get-user | jq -r .User.UserName`
 declare -A keypairs
-keypairs["acute"]="acute yubikey 4"
-keypairs["irl"]="irl yubikey 4"
-keypairs["karsten"]="karsten's key"
+keypairs[acute]="acute yubikey 4"
+keypairs[irl]="irl yubikey 4"
+keypairs[karsten]="karsten's key"
 
 cur_user=$(aws iam get-user | jq -r .User.UserName)
 
-for key in ${!keypairs[@]}; do
-    if [ $key == $cur_user ]; then
-        echo ${keypairs[${key}]};
+for key val in ${(kv)keypairs}; do
+    if [ $key = $cur_user ]; then
+        echo $val;
         break
     fi
 done
