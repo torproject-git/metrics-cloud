ff0412f7495b6df38e7b85bd2163b5521f844659
Iain R. Learmonth <irl@fsfe.org>
2020-03-04 15:49:48 +0000

    a/check: Run updater every 30 minutes

---
+++ ansible/roles/check/files/cpexits.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+CHECK=/srv/check.torproject.org/check
+TORDATA=/srv/check.torproject.org/tordata
+DNSEL=/srv/tordnsel.torproject.org
+NOW=$(date +"%Y-%m-%d-%H-%M-%S")
+
+find $CHECK/data/exit-lists -type f -mtime +1 -delete
+cat $DNSEL/exit-addresses > $CHECK/data/exit-lists/$NOW
+
+find $CHECK/data/consensuses -type f -mtime +1 -delete
+cp $TORDATA/cached-consensus $CHECK/data/consensuses/$NOW-consensus
+
+cat $TORDATA/cached-descriptors $TORDATA/cached-descriptors.new > $CHECK/data/cached-descriptors
+
+cd $CHECK
+scripts/exitips.py -n 1
+kill -s SIGUSR2 `cat check.pid`
